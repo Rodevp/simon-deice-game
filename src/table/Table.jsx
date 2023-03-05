a9b3727c6e7dcbf1540568437c1d5aa00d9d6f3f
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-05 17:05:38 -0500

    generate sequence of colors complete

--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import styles from "./table.module.css"
 import { COLORS } from "../colors"
 
@@ -21,11 +22,47 @@ function Dot({ color, grade, style, action }) {
 }
 
 
+
 function Table() {
+
+    const [sequence, setSequence] = useState([])
+    const [level, setLevel] = useState(2)
+
+    const getColor = () => {
+        const colors = ["yellow", "blue", "red", "green"]
+        const randomNumber = Math.floor(Math.random() * colors.length)
+        return colors[randomNumber]
+    }
+
+    const sequenceColor = () => {
+
+        if (level === 1) {
+            const copy = [...sequence]
+            copy.push(getColor())
+            setSequence(copy)
+        } 
+
+
+        if (level > 1) {
+            let flag = []
+            for (let i = 0; i < level; i++) {
+                flag.push(getColor())
+            }
+            setSequence(flag)
+        }
+
+    }
+
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <div
             className={styles.table}
         >
+            <span>{JSON.stringify(sequence)}</span>
             <div
                 className={styles.contentDot}
             >
@@ -34,38 +71,48 @@ function Table() {
                     grade={"180deg"}
                     style={{
                         top: 10
-                    }}    
+                    }}
+                    action={() => {
+                    }}
                 />
                 <Dot
                     color={COLORS.skyBlue}
                     grade={"270deg"}
                     style={{
                         right: 60
-                    }}    
+                    }}
+                    action={() => {
+                    }}
                 />
                 <Dot
                     color={COLORS.green}
                     grade={"90deg"}
                     style={{
                         left: 60
-                    }}    
+                    }}
+                    action={() => {
+                    }}
                 />
                 <Dot
-                    color={COLORS.red} 
+                    color={COLORS.red}
                     grade={"0deg"}
                     style={{
                         bottom: 10
+                    }}
+                    action={() => {
                     }}
                 />
             </div>
             <button
                 className={styles.button}
-                style={{ 
+                style={{
                     backgroundColor: COLORS.purple,
                     border: `1px solid ${COLORS.white}`,
                     color: COLORS.white
                 }}
-                onClick={() => { }}
+                onClick={() => {
+                    sequenceColor()
+                }}
             >
                 Jugar
             </button>
