48e345125506a8af55f0484f051513efec28af77
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-05 14:46:14 -0500

    add colors

---
+++ src/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+    yellow: "#FCF45D",
+    skyBlue: "#78C6F7",
+    red: "#FF7480",
+    green: "#86E7B8",
+    blueMedium: "#0081C9",
+    blueDark: "#144272",
+    purple: "#3F0071",
+    white: "#FBFEFB"
+}
